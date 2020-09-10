804e463061166c651e08655f6d57ffaaae04be15
Appie Verschoor <xiffy@molecule.nl>
2020-09-10 19:04:11 +0200

    reformat with black I guess the author likes it when I sing 'paint it black'

--- lfm/app.py
+++ lfm/app.py
@@ -9,78 +9,93 @@ app = Flask(__name__)
 config = Config()
 
 
-@app.route('/favicon.ico')
+@app.route("/favicon.ico")
 def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+    return send_from_directory(
+        os.path.join(app.root_path, "static"),
+        "favicon.ico",
+        mimetype="image/vnd.microsoft.icon",
+    )
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route("/", methods=["GET", "POST"])
 def hello_readers():
-    if request.method == 'GET':
-        payload = {'user': 'thexiffy', 'title': 'Example URLs'}
+    if request.method == "GET":
+        payload = {"user": "thexiffy", "title": "Example URLs"}
     else:
-        payload = {'user': request.form['user'], 'title': 'Your URLs'}
-    payload['base_url'] = config.base_url
-    return render_template('home.html', context=payload)
+        payload = {"user": request.form["user"], "title": "Your URLs"}
+    payload["base_url"] = config.base_url
+    return render_template("home.html", context=payload)
 
 
-@app.route('/<user>')
+@app.route("/<user>")
 def recent_tracks(user):
     # /2.0/?method=user.getrecenttracks&user=rj&api_key=YOUR_API_KEY&format=json
-    payload = {'api_key': config.api_key,
-               'method': 'user.getrecenttracks',
-               'user': user,
-               'format': 'json'}
+    payload = {
+        "api_key": config.api_key,
+        "method": "user.getrecenttracks",
+        "user": user,
+        "format": "json",
+    }
     response = requests.get(config.api_base_url, params=payload)
     try:
         json = response.json()
-        if 'recenttracks' in json:
-            context = {'user': user,
-                       'tracks': json['recenttracks'],
-                       'type': 'recent'}
-            return render_template('recent.html', context=context), 200, {"Content-type": "text/xml; charset=utf-8",
-                                                                          "Cache-Control": "max-age=600"}
+        if "recenttracks" in json:
+            context = {"user": user, "tracks": json["recenttracks"], "type": "recent"}
+            return (
+                render_template("recent.html", context=context),
+                200,
+                {
+                    "Content-type": "text/xml; charset=utf-8",
+                    "Cache-Control": "max-age=600",
+                },
+            )
         else:
             abort(404)
     except ValueError:
         return response.content
 
 
-@app.route('/<user>/loved')
+@app.route("/<user>/loved")
 def loved_tracks(user):
-    payload = {'api_key': config.api_key,
-               'method': 'user.getlovedtracks',
-               'user': user,
-               'format': 'json'}
+    payload = {
+        "api_key": config.api_key,
+        "method": "user.getlovedtracks",
+        "user": user,
+        "format": "json",
+    }
     response = requests.get(config.api_base_url, params=payload)
     try:
         json = response.json()
-        if 'lovedtracks' in json:
-            context = {'user': user,
-                       'tracks': json['lovedtracks'],
-                       'type': 'loved'}
-            return render_template('loved.html', context=context), 200, {"Content-type": "text/xml; charset=utf-8",
-                                                                         "Cache-Control": "max-age=600"}
+        if "lovedtracks" in json:
+            context = {"user": user, "tracks": json["lovedtracks"], "type": "loved"}
+            return (
+                render_template("loved.html", context=context),
+                200,
+                {
+                    "Content-type": "text/xml; charset=utf-8",
+                    "Cache-Control": "max-age=600",
+                },
+            )
         else:
             abort(404)
     except ValueError:
         return response.content
 
 
-@app.template_filter('artistlink')
+@app.template_filter("artistlink")
 def make_artistlink(url):
-    return '/'.join(url.split('/')[:-2])
+    return "/".join(url.split("/")[:-2])
 
 
-@app.template_filter('rfc822_date')
+@app.template_filter("rfc822_date")
 def rfc822_date(datestring):
     if not datestring:
         nowdt = datetime.utcnow()
         return utils.format_datetime(nowdt)
-    dt = datetime.strptime(datestring, '%d %b %Y, %H:%M')
+    dt = datetime.strptime(datestring, "%d %b %Y, %H:%M")
     return utils.format_datetime(dt)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
